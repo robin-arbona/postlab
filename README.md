cc8e8f0974d7b69eb51fdce010bda2f3d149c85b
Robin Arbona <robin@i-wantit.com>
2022-03-28 15:29:28 +0200

    feat: get info from a repo

---
+++ README.md
@@ -0,0 +1,13 @@
+# installation
+
+create .env with a key TOKEN=YOUR_GITHUB_TOKEN
+
+```bash
+yarn
+```
+
+# Launch project
+
+```bash
+node index.js
+```
--- index.js
+++ index.js
@@ -8,14 +8,28 @@ const {
   } = await octokit.rest.users.getAuthenticated();
 
 
-  console.log("Hello, %s", login);
+console.log("Hello, %s", login);
 
- const repo =  await octokit.rest.repos.get({
-      repo: "IAMDinosaur",
-      owner: 'ivanseidel'
-  })
+const repo = 'IAMDinosaur';
+const owner = 'ivanseidel';
 
-  // OK description, langages de programmation, licences, versions, mots clés, liens web …
-  // NOK
+const { data : {license, topics, language, description}} =  await octokit.rest.repos.get({
+    repo,
+    owner
+})
 
-  console.log(repo)
+// licences, versions, mots clés, liens web …
+// NOK collaborators
+
+console.log(license, topics, language, description)
+
+// get branches
+const { data : branches} = await octokit.request(`GET /repos/${owner}/${repo}/branches`)
+console.log(branches)
+
+// get commits descriptions
+const { data: {commit}} = await octokit.request(`https://api.github.com/repos/ivanseidel/IAMDinosaur/commits/a3737740ad6e18eb3260f9c32d1070102abc4f5e`)
+console.log(commit)
+
+
+
