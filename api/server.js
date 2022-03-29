61838705adeb5ead637147164a48ec230530ee38
Robin Arbona <robin@i-wantit.com>
2022-03-29 15:38:09 +0200

    feat: api webhook creation

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var xhub = require("express-x-hub");
 
 //Const
-var xhubSecret = "SHA256:mjr+czigDZoIilGDd+ors1feZO1eg6L6h6/fhj+14HY";
+var xhubSecret = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCloFeBpIjocE16I9hycAcUas3IrjsZwnXN1OQW7YNmCf+AjuwQPbBiRaBRAhfGPXitRhnsxxDjZ1XAGg6NJCQP92ZJlJ/axN9CwYmc0F5F8n2vYoqXMNnDnPQfKuKANCHFHCnjlo0ajfIyiZcyBDukWXY5lSd8GzJj7Q6ZucQyTz24okz1Tub8Imr4eC5VOAhAK5GoxtfI6+MqexYVNu9rfavBKDBRne7z4IVOsktMlNQntQ1y+LUPFHiko42JS681gZ4iile0a5anV9i+xk8WXyfoliEALxR4hEG36Qgith70xSTG0ykbD3+0F1ogNjkf6NKOkUrawc0l5QRzvX55hNCwIRmxw2Gg1v6ilppl9cinKnkoEbfTM4WP+UKh5fjHesd2ff/btAc/WQbMFMLykz8TS4a7+TCKx0uccUxE6DjltgBIEgDw5rlKWxnN55pbK+iRlUvRHnD/mjSwBqYf/XDu1fcJ2Mmr/itU/pViJdCfQu2qjYJVrMcP+sAHT88= robinarbona@MacBook-Pro-de-Robin.local";
 var port = "8085";
 var host = "127.0.0.1";
 
