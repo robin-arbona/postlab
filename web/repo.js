c0c76041578165a247f81a45f8332fb6b807d4c9
Robin Arbona <robin@i-wantit.com>
2022-03-29 15:17:31 +0200

    feat: formating web

--- web/repo.js
+++ web/repo.js
@@ -9,35 +9,49 @@
     const file = `${repoInfo.repo}_${repoInfo.owner}.json`
 
 
-    const jsonRepo = await fetch(`../repo/${file}`).then(r=>r.json())
+    const {commits, info} = await fetch(`../repo/${file}`).then(r=>r.json())
 
-    console.log(jsonRepo)
+    console.log(info)
+    // Project info
+    for (const key in info) {
+        if (Object.hasOwnProperty.call(info, key)) {
+            const el = document.querySelector(`#${key}`)
+            if(el){
+                el.innerHTML = typeof info[key] === 'object' ? JSON.stringify(info[key]) : info[key]
+            }
+        }
+    }
 
+    // --- TimeLine ---
     const dataSet = [];
 
     let id = 0;
-    for (const sha in jsonRepo.commits) {
+    for (const sha in commits) {
         id++
-        const { commit } = jsonRepo.commits[sha]
+        const { commit } = commits[sha]
         dataSet.push({
-        id,
-        content: commit.author.name,
-        start: commit.author.date.slice(0,10)
+            id,
+            content: commit.author.name,
+            start: commit.author.date.slice(0,10),
+            sha
         })
     }
 
-    console.log(dataSet)
-
     // DOM element where the Timeline will be attached
-    var container = document.getElementById('visualization');
+    const container = document.getElementById('visualization');
 
     // Create a DataSet (allows two way data-binding)
-    var items = new vis.DataSet(dataSet);
+    const items = new vis.DataSet(dataSet);
 
     // Configuration for the Timeline
-    var options = {};
+    const options = {};
 
     // Create a Timeline
-    var timeline = new vis.Timeline(container, items, options);
+    const timeline = new vis.Timeline(container, items, options);
+
+    document.getElementById('visualization').onclick = function (event) {
+        const props = timeline.getEventProperties(event)
+        console.log(commits[dataSet[props.item].sha]);
+      }
 })()
 
