d5944de981892f5bf27fab51eb70d3a281499894
Robin Arbona <robin@i-wantit.com>
2022-03-29 09:25:06 +0200

    feat: get commits, write all info in .json file

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 import { Octokit, App } from "octokit";
 import 'dotenv/config';
+import * as fs from 'fs';
 
 const octokit = new Octokit({ auth: process.env.TOKEN });
-
-const {
-    data: { login },
-  } = await octokit.rest.users.getAuthenticated();
-
-
-console.log("Hello, %s", login);
 
 const repo = 'IAMDinosaur';
 const owner = 'ivanseidel';
 
-const { data : {license, topics, language, description}} =  await octokit.rest.repos.get({
+console.log( repo, owner)
+
+const { data : {license, topics, language, description}}  =  await octokit.rest.repos.get({
     repo,
     owner
 })
+
 
 // licences, versions, mots clés, liens web …
 // NOK collaborators
@@ -25,11 +22,60 @@ console.log(license, topics, language, description)
 
 // get branches
 const { data : branches} = await octokit.request(`GET /repos/${owner}/${repo}/branches`)
-console.log(branches)
 
-// get commits descriptions
-const { data: {commit}} = await octokit.request(`https://api.github.com/repos/ivanseidel/IAMDinosaur/commits/a3737740ad6e18eb3260f9c32d1070102abc4f5e`)
-console.log(commit)
+// Get un commit and add it to commits list object
+const getCommit = async (sha, commits) => {
+  const {status, data} = await octokit.request(`https://api.github.com/repos/${owner}/${repo}/commits/${sha}`)
+  if(status === 200){
+    commits[sha] = data;
+    for(const parent of data?.parents){
+      if(!(parent.sha in commits) || Object.keys(commits[sha]).length === 0){
+        await getCommit(parent.sha,commits)
+      }
+    }
+  }
+} 
+
+
+const getCommits = async (commits) =>{
+  for (const sha in commits) {
+    await getCommit(sha,commits)
+  }
+}
+
+
+(async()=>{
+
+  const commits = {};
+
+  for (const branche of branches) {
+    commits[branche.commit.sha] = {}  
+  }
+
+  await getCommits(commits);
+
+  console.log('commits',Object.keys(commits).length);
+
+  const repository = {
+    repo,
+    owner,
+    license,
+    topics,
+    language,
+    description,
+    commits
+  }
+  
+  const repoJson = JSON.stringify(repository);
+
+  fs.writeFile(`${repo}_${owner}.json`, repoJson, 'utf8', ()=>{console.log('end of writing')});
+
+})()
+
+
+
+
+
 
 
 
